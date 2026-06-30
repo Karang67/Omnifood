12343b222c2d344f5bc7b745ffc732d7db4ec847
Student karan <Studentkarangehlot5686@gmail.com>
2026-06-30 21:54:46 +0530

    fix: update Content Security Policy (CSP) to allow Google GIS scripts and frames

--- app.mjs
+++ app.mjs
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
     res.setHeader("X-Frame-Options", "DENY");
     res.setHeader("X-XSS-Protection", "1; mode=block");
     res.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
-    res.setHeader("Content-Security-Policy", "default-src 'self' https://unpkg.com https://fonts.googleapis.com https://fonts.gstatic.com; img-src 'self' data: https://unpkg.com https://*.openstreetmap.org; style-src 'self' 'unsafe-inline' https://unpkg.com https://fonts.googleapis.com; script-src 'self' 'unsafe-inline' https://unpkg.com https://cdn.jsdelivr.net; font-src 'self' https://unpkg.com https://fonts.gstatic.com; connect-src 'self'");
+    res.setHeader("Content-Security-Policy", "default-src 'self' https://unpkg.com https://fonts.googleapis.com https://fonts.gstatic.com; img-src 'self' data: https://unpkg.com https://*.openstreetmap.org; style-src 'self' 'unsafe-inline' https://unpkg.com https://fonts.googleapis.com https://accounts.google.com/gsi/style; script-src 'self' 'unsafe-inline' https://unpkg.com https://cdn.jsdelivr.net https://accounts.google.com/gsi/client; font-src 'self' https://unpkg.com https://fonts.gstatic.com; connect-src 'self' https://accounts.google.com/gsi/; frame-src 'self' https://accounts.google.com/");
     next();
 });
 
